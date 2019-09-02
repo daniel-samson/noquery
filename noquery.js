7675471db551df0473dda1a226b0853f57e2e327
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2019-09-02 23:38:41 +0100

    Create noquery.js

---
+++ noquery.js
@@ -0,0 +1,42 @@
+function $ready(callback) {
+  return window.addEventListener('load', callback);
+}
+
+function $1(selector) {
+  return document.querySelector(selector);
+}
+
+function $all(selector) {
+  return document.querySelectorAll(selector);
+}
+
+function $show(selector) {
+  $all(selector).forEach(element => {
+    element.classList.remove('hidden');
+  });
+}
+
+function $hide(selector) {
+  $all(selector).forEach(function (element) {
+    element.classList.add('hidden');
+  });
+}
+
+function $toggleClassName(selector, classNameNeedle, classNameReplacement) {
+  $all(selector).forEach(function (element) {
+    if (element.classList.contains(classNameNeedle)) {
+      element.classList.remove(classNameNeedle);
+    } else {
+      element.classList.add((classNameReplacement) ? classNameReplacement : classNameNeedle);
+    }
+  });
+}
+
+function $stop(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function $listen(selector, event, listener) {
+  return $1(selector).addEventListener(event, listener);
+}
